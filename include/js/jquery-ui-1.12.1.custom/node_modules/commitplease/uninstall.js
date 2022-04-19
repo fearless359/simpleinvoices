b98fae5ef7abcf155139f5784703bab0c7ad1f2c
fearless359 <Rich@FearlessTechComp.com>
2022-04-19 09:42:34 -0700

    Delete node library from jquery-ui directory.

--- include/js/jquery-ui-1.12.1.custom/node_modules/commitplease/uninstall.js
+++
@@ -1,8 +0,0 @@
-var setup = require( "./setup" );
-
-if ( setup.selfmadeHook ) {
-	console.log( "Found a hook installed by commitplease, removing" );
-	setup.destroy();
-} else {
-	console.log( "Didn't find a commit-msg hook installed by this module, doing nothing" );
-}
